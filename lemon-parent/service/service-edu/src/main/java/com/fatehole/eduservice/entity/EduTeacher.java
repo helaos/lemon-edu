420cd5b754259a072a96f471de8c0b0a733292fb
文泽 <cifanse@outlook.com>
2020-11-09 17:26:28 +0800

    Add, delete, modify and check the instructor module 🥡

--- lemon-parent/service/service-edu/src/main/java/com/fatehole/eduservice/entity/EduTeacher.java
+++ lemon-parent/service/service-edu/src/main/java/com/fatehole/eduservice/entity/EduTeacher.java
@@ -1,11 +1,11 @@
 package com.fatehole.eduservice.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.util.Date;
-import com.baomidou.mybatisplus.annotation.TableId;
+
 import java.io.Serializable;
 
-import com.baomidou.mybatisplus.annotation.TableLogic;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -28,8 +28,8 @@ public class EduTeacher implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @TableId(value = "id", type = IdType.ID_WORKER_STR)
     @ApiModelProperty(value = "讲师ID")
-    @TableId(value = "id", type = IdType.ID_WORKER)
     private String id;
 
     @ApiModelProperty(value = "讲师姓名")
@@ -54,9 +54,11 @@ public class EduTeacher implements Serializable {
     @ApiModelProperty(value = "逻辑删除 1（true）已删除， 0（false）未删除")
     private Boolean isDeleted;
 
+    @TableField(fill = FieldFill.INSERT)
     @ApiModelProperty(value = "创建时间")
     private Date createTime;
 
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     @ApiModelProperty(value = "更新时间")
     private Date updateTime;
 
--- lemon-parent/service/service-edu/src/main/java/com/fatehole/eduservice/service/EduTeacherService.java
+++ lemon-parent/service/service-edu/src/main/java/com/fatehole/eduservice/service/EduTeacherService.java
@@ -1,7 +1,9 @@
 package com.fatehole.eduservice.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.fatehole.eduservice.entity.EduTeacher;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.fatehole.eduservice.entity.vo.TeacherQuery;
 
 /**
  * <p>
@@ -12,5 +14,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-11-08
  */
 public interface EduTeacherService extends IService<EduTeacher> {
+
+    /**
+     * 根据具体的条件和信息分页查询
+     * @param pageParam 分页信息
+     * @param teacherQuery 查询条件
+     */
+    void pageQuery(Page<EduTeacher> pageParam, TeacherQuery teacherQuery);
 
 }
